94f8dc188435ca9834e5e9c00f0c77c98054b9a8
Leon² <75674735+VitaAetaerna@users.noreply.github.com>
2025-02-22 13:26:52 +0100

    Create Values.cs

---
+++ Assault Cube/SimpleHack/Values.cs
@@ -0,0 +1,7 @@
+public static class Values{
+	public static IntPtr Health = EC; // localPlayerPointer + EC == Health
+	public static IntPtr Ammo =  new IntPtr();
+	public static IntPtr Speed =  new IntPtr();
+	public static IntPtr Armor =  new IntPtr();
+	public static IntPtr Grenades =  new IntPtr();
+}
