6f6e44cd4cf7ed060b3426904ba9b7bf15baa58f
Leon² <75674735+VitaAetaerna@users.noreply.github.com>
2025-02-22 13:24:26 +0100

    Update Entry.cs

--- Assault Cube/SimpleHack/Entry.cs
+++ Assault Cube/SimpleHack/Entry.cs
@@ -1,1 +1,8 @@
-
+public static class Entry{
+	
+	public static void Main(){
+		Console.WriteLine("This is made for Educational Purposes only!");
+		Proc _process = new Proc();
+		_process.GetProcessInfo();
+	}
+}
